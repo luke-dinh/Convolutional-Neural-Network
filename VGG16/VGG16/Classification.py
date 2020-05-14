498519bc37fedb1ece199908f7e5d634f5bd4cc9
luke-dinh <dinhphuoclockt@gmail.com>
2020-05-14 13:17:18 +0700

    create new files

---
+++ VGG16/VGG16/Classification.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import numpy as np
+
+model = VGG16(weights='imagenet', include_top=False)
+
+img_path = 'd:/clock.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+features = model.predict(x)
+print(features.shape)
+print(features)
+
